f0d2e65c3d550e7b1cd42552c2f50a3cf6f26e66
parastou-hj <jalilian.parastou91@gmail.com>
2025-04-06 16:56:50 +0330

    almost-web-done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper('.swiper', {
+var swiper = new Swiper('#my-swiper', {
     loop: true,
     speed: 3000,
     // autoplay: {
@@ -7,21 +7,39 @@ var swiper = new Swiper('.swiper', {
     //     // stopOnLastSlide:true
     // },
     effect: 'coverflow',
+    grabCursor: true,
     centeredSlides: true,
     slidesPerView: 'auto',
-    loop: true,
     coverflowEffect: {
-      rotate: 0,
-      stretch: 120,   // اسلایدها نزدیک‌تر می‌شن
-      depth: 200,     // عمق کم برای افکت ملایم
-      modifier: 1,
-      slideShadows: false
+        rotate: 0,
+        stretch: 150,
+        depth: 200,
+        modifier: -0.5,
+        slideShadows: true,
     },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable:true
+      },
 
   watchSlidesProgress:true,
  
 });
-const swiperr = document.querySelector('.swiper').swiper;
+const swiperr = document.querySelector('#my-swiper').swiper;
+
+    let nextbtn=document.querySelector('.slider-right');
+    nextbtn.addEventListener('click',function(){
+        swiperr.slidePrev();
+    })
+    let prevbtn=document.querySelector('.slider-left');
+    prevbtn.addEventListener('click',function(){
+        swiperr.slideNext();
+    })
+    // const heroSlider=document.querySelector('#my-swiper');
+    // heroSlider.addEventListener('mouseenter', function(){
+    //     // swiper.stop();
+    // })
 
 
 //owl-pro
@@ -48,7 +66,7 @@ $(function(){
                   992:{
                       items: 9
                   },
-                  1600:{
+                  1200:{
                       items: 9.5
                   }
               }
@@ -61,7 +79,7 @@ $(function(){
   $('#sale-owl-one').owlCarousel({
               rtl: true,
               loop: true,
-              margin: 10,
+            //   margin: 10,
               nav:false,
               dots:false,
               // autoplay:true,
@@ -80,9 +98,216 @@ $(function(){
                   992:{
                       items: 4
                   },
-                  1600:{
+                  1200:{
                       items: 4.5
                   }
               }
   })
 })
+$('#one-right').click(function() {
+    $('#sale-owl-one').trigger('prev.owl.carousel');
+});
+
+$('#one-left').click(function() {
+    $('#sale-owl-one').trigger('next.owl.carousel');
+});
+
+//product-swiper
+var swiper = new Swiper('#my-swiper-2', {
+    loop: true,
+    speed: 3000,
+    // autoplay: {
+    //     delay: 1000,
+    //     reverseDirection:true,
+    //     // stopOnLastSlide:true
+    // },
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 80,
+        depth: 0,
+        modifier: -0.5,
+        slideShadows: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable:true
+      },
+
+  watchSlidesProgress:true,
+ 
+});
+
+//most sale
+$(function(){
+    $('#sale-owl-two').owlCarousel({
+                rtl: true,
+                loop: true,
+                margin: 5,
+                nav:false,
+                dots:false,
+                autoplay:true,
+                autoplayTimeout: 3000,
+                autoplayHoverPause: true,
+                responsive:{
+                    0:{
+                        items: 2
+                    },
+                    480:{
+                        items: 2.5
+                    },
+                    576:{
+                        items: 3
+                    },
+                    680:{
+                        items: 3.5
+                    },
+                    768:{
+                        items: 4
+                    },
+                    860:{
+                        items: 4.5
+                    },
+                    1060:{
+                        items: 5,
+                    },
+                  
+                }
+    })
+})
+$('#one-right').click(function() {
+    $('#sale-owl-two').trigger('prev.owl.carousel');
+});
+
+$('#one-left').click(function() {
+    $('#sale-owl-two').trigger('next.owl.carousel');
+});
+
+//category
+$(function(){
+    $('#owl-category').owlCarousel({
+                rtl: true,
+                loop: true,
+                margin: 5,
+                nav:false,
+                dots:false,
+                responsive:{
+                    0:{
+                        items: 2
+                    },
+                    480:{
+                        items: 2.5
+                    },
+                    576:{
+                        items: 3
+                    },
+                    680:{
+                        items: 3.5
+                    },
+                    768:{
+                        items: 4
+                    },
+                    860:{
+                        items: 4.5
+                    },
+                    1060:{
+                        items: 6,
+                    },
+                  
+                }
+    })
+})
+
+//most-sale-2
+$(function(){
+    $('#sale-owl-three').owlCarousel({
+                rtl: true,
+                loop: true,
+                margin: 5,
+                nav:false,
+                dots:false,
+                autoplay:true,
+                autoplayTimeout: 3000,
+                autoplayHoverPause: true,
+                responsive:{
+                    0:{
+                        items: 2
+                    },
+                    480:{
+                        items: 2.5
+                    },
+                    576:{
+                        items: 3
+                    },
+                    680:{
+                        items: 3.5
+                    },
+                    768:{
+                        items: 4
+                    },
+                    860:{
+                        items: 4.5
+                    },
+                    1060:{
+                        items: 5,
+                    },
+                  
+                }
+    })
+})
+$('#one-right').click(function() {
+    $('#sale-owl-two').trigger('prev.owl.carousel');
+});
+
+$('#one-left').click(function() {
+    $('#sale-owl-two').trigger('next.owl.carousel');
+});
+
+//most-sale-2
+$(function(){
+    $('#sale-owl-four').owlCarousel({
+                rtl: true,
+                loop: true,
+                margin: 5,
+                nav:false,
+                dots:false,
+                autoplay:true,
+                autoplayTimeout: 3000,
+                autoplayHoverPause: true,
+                responsive:{
+                    0:{
+                        items: 2
+                    },
+                    480:{
+                        items: 2.5
+                    },
+                    576:{
+                        items: 3
+                    },
+                    680:{
+                        items: 3.5
+                    },
+                    768:{
+                        items: 4
+                    },
+                    860:{
+                        items: 4.5
+                    },
+                    1060:{
+                        items: 5,
+                    },
+                  
+                }
+    })
+})
+$('#one-right').click(function() {
+    $('#sale-owl-two').trigger('prev.owl.carousel');
+});
+
+$('#one-left').click(function() {
+    $('#sale-owl-two').trigger('next.owl.carousel');
+});
